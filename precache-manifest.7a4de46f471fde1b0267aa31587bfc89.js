af6d7bd0d60e3b34080dfa6bbbed7f4eb736f917
AbdelTrochez <abdel.atr@gmail.com>
2020-11-27 04:59:09 -0600

    Updates

---
+++ precache-manifest.7a4de46f471fde1b0267aa31587bfc89.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e62d5709fd2ce8445ab2cd1a2f08998",
+    "url": "/actividadsemanal_react/index.html"
+  },
+  {
+    "revision": "4beb9020fbeba8825cf1",
+    "url": "/actividadsemanal_react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4178b81f20eee6ebfb3",
+    "url": "/actividadsemanal_react/static/js/2.8ef7aa3b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/actividadsemanal_react/static/js/2.8ef7aa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4beb9020fbeba8825cf1",
+    "url": "/actividadsemanal_react/static/js/main.85cbe702.chunk.js"
+  },
+  {
+    "revision": "ab49b04f4426c1337d27",
+    "url": "/actividadsemanal_react/static/js/runtime-main.bf4357f2.js"
+  },
+  {
+    "revision": "af37b25557017e69cb04a936f3538295",
+    "url": "/actividadsemanal_react/static/media/bebe.af37b255.jpg"
+  },
+  {
+    "revision": "aab2f4970b097110bda1eaf666b880b5",
+    "url": "/actividadsemanal_react/static/media/carousel1.aab2f497.jpg"
+  },
+  {
+    "revision": "5a9bdbc069cad39b5de59d9cf8a51fcf",
+    "url": "/actividadsemanal_react/static/media/carousel2.5a9bdbc0.jpg"
+  },
+  {
+    "revision": "7924a82275715f8cc3ca6dee25a98272",
+    "url": "/actividadsemanal_react/static/media/carousel3.7924a822.jpg"
+  }
+]);
